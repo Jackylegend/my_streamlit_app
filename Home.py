ac283e3fedfab4ebdaf3be331281e2145217234f
Jacky Liu <fanj.zhang@gmail.com>
2024-05-29 23:14:57 -0400

    GDP Page is done

---
+++ Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import streamlit_antd_components as sac
+
+
+
+
--- dataloader.py
+++ dataloader.py
@@ -8,25 +8,14 @@ import pandas as pd
 @st.cache_data
 def load_gdp_data():
     gdp_df = pd.read_excel('dataset/gdp.xlsx')
-
-    gdp_df['Date'] = pd.to_datetime(gdp_df['Date']).dt.strftime('%m-%d-%Y')
-
+    gdp_df = gdp_df[(gdp_df['Index_Level'] == 0) | (gdp_df['Index_Level'] == 1)]
+    gdp_df['Date'] = pd.to_datetime(gdp_df['Date']).dt.strftime('%Y-%m-%d')
     freq = gdp_df['Frequency'].unique().tolist()
     type = gdp_df['Type'].unique().tolist()
     unit = gdp_df['Unit'].unique().tolist()
+    var_names = gdp_df['Name'].unique().tolist()
+    return gdp_df, freq, type, unit, var_names
 
-
-
-
-    # Filter Variables
-    # index_level = gdp['Index_Level'].unique()
-    # index_mapping = gdp.loc[gdp['Index_Level'].isin(index_level), ['Index_Mapping']]
-    # parent_level = gdp['Index_Mapping'].unique()
-    # type_ = gdp['Type'].unique()
-    # freq = gdp['Frequency'].unique()
-    # unit = gdp['Unit'].unique()
-    # data_col =['DATE','Name','real_pct_Quarter','nominal_GDP_Quarter','nominal_pct_Quarter','real_GDP_Quarter','nominal_GDP_Annual','nominal_pct_Annual','real_GDP_Annual','real_pct_Annual']
-    return gdp_df, freq, type, unit
 
 #---------------------------------------------------------------------------------------------------------------------------------------
 
